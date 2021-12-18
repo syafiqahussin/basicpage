6143e2a33f15345b8aaa33c71ce646db0abbb57f
syafiqahussin <96322724+syafiqahussin@users.noreply.github.com>
2021-12-19 06:03:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter</title>
+        <title>Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Testing" />
+        <Header title="NURUL SYAFIQA" />
         <p className="description">
-          Get started by editing <code>pages/Index.js</code>
+          What you looking this my page.
         </p>
       </main>
 
